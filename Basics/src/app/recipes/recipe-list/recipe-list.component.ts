b27c4e7de037e10510ae0c3171efd453ec893fa0
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-06-23 16:42:01 -0700

    Structured components html files

--- Basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Dough, tomato sauce, cheese', 'https://files.liveworksheets.com/def_files/2020/10/3/1003050249676828/1003050249676828001.jpg')
+  ];
 
   constructor() { }
 
--- Basics/src/app/shopping-list/shopping-list.component.ts
+++ Basics/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Dough', 1),
+    new Ingredient('Cheese', 4),
+    new Ingredient('Tomato Sauce', 1)
+  ];
   constructor() { }
 
   ngOnInit(): void {
