e46bd8b73e6942b5c235822df124105e628a912d
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-06-27 17:58:30 -0700

    added databinding and directives

--- Basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Pizza', 'Dough, tomato sauce, cheese', 'https://files.liveworksheets.com/def_files/2020/10/3/1003050249676828/1003050249676828001.jpg')
   ];
@@ -14,6 +16,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
