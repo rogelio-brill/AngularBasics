b7838c820d7aac8a0f537dc942f248c1832339d2
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-07-01 17:04:52 -0700

    completed forms

--- Basics/src/app/shopping-list/shopping-list.service.ts
+++ Basics/src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,8 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Chicken', 3)
@@ -12,6 +14,10 @@ export class ShoppingListService {
       getIngredients() {
         return this.ingredients.slice();
       } 
+
+      getIngredient(index: number) {
+        return this.ingredients[index];
+      }
 
       addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
@@ -25,4 +31,15 @@ export class ShoppingListService {
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
       }
+
+      updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
 }
